fd8854d07e7dcae46ebb6d17fa3b7a9d9ff80b4c
juanramirezwebdev <juanluisra11@gmail.com>
2024-01-20 13:55:08 +1300

    first commit

---
+++ src/routes/contact.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { FileRoute } from '@tanstack/react-router'
+
+export const Route = new FileRoute('/contact').createRoute({
+  component: ContactComponent,
+})
+
+function ContactComponent() {
+  return (
+    <div className="p-2">
+      <h3>Contact</h3>
+    </div>
+  )
+}
